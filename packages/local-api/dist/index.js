c6676aa64fc4aa81cf0c711eb2018ee344f93d96
jasonLapina <lapina.jason@gmail.com>
2023-07-30 01:57:01 +0800

    seperate into packages, integrate lerna

---
+++ packages/local-api/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serve = void 0;
+const serve = (port, filename, dir) => {
+    console.log(port, filename, dir);
+};
+exports.serve = serve;
